f79fb19dde47568aac5f86300c7f862b3e533ec3
DSnoddy <douglassnoddyjr@gmail.com>
2021-04-16 21:37:21 -0800

    updated index.html with service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = 'static-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
@@ -42,7 +42,7 @@ self.addEventListener('activate', function (evt) {
         })
     );
 
-    self.ClientRectList.claim();
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', function (evt) {
